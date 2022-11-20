2c531b1d8120183a72f7ade4e8650cbe5c3715ef
lulalend <yanakl@list.ru>
2022-11-20 13:55:26 +0300

    add custom tag

---
+++ src/main/java/ru/itmo/lab_2/jstl/ChangeFormatTag.java
@@ -0,0 +1,35 @@
+package ru.itmo.lab_2.jstl;
+
+import javax.servlet.jsp.JspContext;
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.tagext.JspFragment;
+import javax.servlet.jsp.tagext.JspTag;
+import javax.servlet.jsp.tagext.SimpleTag;
+import java.io.IOException;
+
+public class ChangeFormatTag implements SimpleTag {
+    @Override
+    public void doTag() throws JspException, IOException {
+
+    }
+
+    @Override
+    public void setParent(JspTag parent) {
+
+    }
+
+    @Override
+    public JspTag getParent() {
+        return null;
+    }
+
+    @Override
+    public void setJspContext(JspContext pc) {
+
+    }
+
+    @Override
+    public void setJspBody(JspFragment jspBody) {
+
+    }
+}
